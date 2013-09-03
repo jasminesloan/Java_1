e5a3632acac37ef90002e46990ee8b013ba6a67f
jasminesloan <jasminesloan@fullsail.edu>
2013-09-03 12:12:09 -0400

    Added jAutodoc Header

--- Week1_Java/gen/com/jasminesloan/week1_java/BuildConfig.java
+++ Week1_Java/gen/com/jasminesloan/week1_java/BuildConfig.java
@@ -1,4 +1,12 @@
-/** Automatically generated file. DO NOT MODIFY */
+/*
+ * project  Week1_Java
+ * 
+ * package  com.jasminesloan.week1_java
+ * 
+ * author  Jasmine Sloan
+ * 
+ * date  Sep 3, 2013
+ */
 package com.jasminesloan.week1_java;
 
 public final class BuildConfig {
