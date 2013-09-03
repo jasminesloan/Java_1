c79736f09bb0ce33435e7efa5948474b64ae58ac
jasminesloan <jasminesloan@fullsail.edu>
2013-09-03 12:13:30 -0400

    Added APK

--- Week1_Java/gen/com/jasminesloan/week1_java/BuildConfig.java
+++ Week1_Java/gen/com/jasminesloan/week1_java/BuildConfig.java
@@ -1,12 +1,4 @@
-/*
- * project  Week1_Java
- * 
- * package  com.jasminesloan.week1_java
- * 
- * author  Jasmine Sloan
- * 
- * date  Sep 3, 2013
- */
+/** Automatically generated file. DO NOT MODIFY */
 package com.jasminesloan.week1_java;
 
 public final class BuildConfig {
